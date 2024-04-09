a056664334f3ea2d538c7976b3058de73ec81ac6
tamirtbd <tamir@name-tbd.com>
2024-04-09 14:55:53 +0300

    Adding host to config to allow docker to listen to outside connections

--- simple_app.py
+++ simple_app.py
@@ -76,4 +76,4 @@ def run_script():
     return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-  app.run(debug=False, port=8089)
+  app.run(host=config['host'], debug=False, port=8089)
