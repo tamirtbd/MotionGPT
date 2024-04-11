de611a9b75f2f3f58c10701451ef22de6826793f
tamirtbd <tamir@name-tbd.com>
2024-04-11 12:04:37 +0300

    Upadting blender deps, adding mode to blender script

--- simple_app.py
+++ simple_app.py
@@ -89,7 +89,7 @@ def run_script():
     # Blender script
     nicename = prompt.replace(" ", "_")
     usd_export_path = Path(outputdir).joinpath(f'{nicename}.usd')
-    command = f"{blender_bin_path} {blender_scene_path} -b -P {blender_script_path} --npy_pose_file_path {npyfile_path} --export_path {usd_export_path}"
+    command = f"{blender_bin_path} {blender_scene_path} -b -P {blender_script_path} --npy_pose_file_path {npyfile_path} --export_path {usd_export_path} --mode {rig_type}"
 
     print("Running Blender with following command:")
     print(command)
