5d5dce9d132aeb0cb7c2dd3aebdba6f3d3b1e919
tamirtbd <tamir@name-tbd.com>
2024-04-11 11:43:20 +0300

    Updated to support miaxmo rig and uploaded base pose

--- scripts/blender_npy2usd.py
+++ scripts/blender_npy2usd.py
@@ -5,6 +5,11 @@ from mathutils import Matrix
 
 humanml3d_joints = [ 'root', 'RH', 'LH', 'BP', 'RK', 'LK', 'BT', 'RMrot', 'LMrot', 'BLN', 'RF', 'LF', 'BMN', 'RSI', 'LSI', 'BUN', 'RS', 'LS', 'RE', 'LE', 'RW', 'LW' ]
 smplh_joints = 'pelvis, right_hip, left_hip, spine1, right_knee, left_knee, spine3, right_ankle, left_ankle, neck, right_foot, left_foot, jaw, right_collar, left_collar, head, right_shoulder, left_shoulder, right_elbow, left_elbow, right_wrist, left_wrist'.split(', ')
+miaxmo_joints = [
+    'mixamorig_Hips', 'mixamorig_RightUpLeg', 'mixamorig_LeftUpLeg', 'mixamorig_Spine', 'mixamorig_RightLeg', 'mixamorig_LeftLeg', 'mixamorig_Spine2',
+    'mixamorig_RightFoot', 'mixamorig_LeftFoot', 'mixamorig_Neck', 'mixamorig_RightToeBase', 'mixamorig_LeftToeBase', '', '', '',
+    'mixamorig_Head', 'mixamorig_RightShoulder', 'mixamorig_LeftShoulder', 'mixamorig_RightArm', 'mixamorig_LeftArm', 'mixamorig_RightForeArm', 'mixamorig_LeftForeArm'
+]
 
 # Get Numpy File Path from cli keyword arguments
 clean_argv = argv[5:]
@@ -17,9 +22,17 @@ print(args)
 fp = args['npy_pose_file_path']
 a = np.load(fp)
 
+mode = args.get('mode', 'smplx')
+skel_obj_name = 'SMPLX-male'
+
+joints = smplh_joints
+if mode != 'smplx':
+    joints = miaxmo_joints
+    skel_obj_name = 'mixamorig_Root'
+
 # Select skeleton
 C = bpy.context
-skel = bpy.data.objects['SMPLX-male']
+skel = bpy.data.objects[skel_obj_name]
 skel.select_set(True)
 
 # Minor transformations on poses
@@ -30,15 +43,16 @@ aroot[:, 1] = -aroot[:, 1]
 for i, f in enumerate( a ):
     n = i+1
     
-    for r, name in zip( f, smplh_joints ):
-        pb = skel.pose.bones[name]
-        rotmat = Matrix( r )
-        rotmat = rotmat.inverted()
-        quat = rotmat.to_quaternion()
-        quat.x *= -1
-        
-        pb.rotation_quaternion = quat
-        pb.keyframe_insert('rotation_quaternion', frame=n)
+    for r, name in zip( f, joints ):
+        if name in skel.pose.bones:
+            pb = skel.pose.bones[name]
+            rotmat = Matrix( r )
+            rotmat = rotmat.inverted()
+            quat = rotmat.to_quaternion()
+            quat.x *= -1
+
+            pb.rotation_quaternion = quat
+            pb.keyframe_insert('rotation_quaternion', frame=n)
 
 # Final adjustments to orient character    
 bpy.ops.transform.rotate(value=3.14159, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
